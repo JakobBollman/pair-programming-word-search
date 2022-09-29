b92380c0783f1a1f435d02929186647caf53706a
JakobBollman <jakob@bollman.ca>
2022-09-28 19:38:41 -0600

    Complete

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,34 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+  if (!word) {
+    return undefined;
+  }
+  if (letters.length < 1) {
+    return undefined;
+  }
+  const horizontalJoin = letters.map((ls) => ls.join(""));
+  for (l of horizontalJoin) {
+    if (l.includes(word)) {
+      return true;
     }
-}
+  }
+  const verticalJoin = transpose(letters).map((ls) => ls.join(""));
+  for (l of verticalJoin) {
+    if (l.includes(word)) {
+      return true;
+    }
+  }
+  return false;
+};
 
-module.exports = wordSearch
+const transpose = function (matrix) {
+  let newMatrix = [];
+  for (const row in matrix[0]) {
+    let col = [];
+    for (const column of matrix) {
+      col.push(column[row]);
+    }
+    newMatrix.push(col);
+  }
+  return newMatrix;
+};
+module.exports = wordSearch;
